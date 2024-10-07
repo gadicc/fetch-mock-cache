e99d1a981a35560923f587b07fd2774ee718086a
Gadi Cohen <dragon@wastelands.net>
2024-10-07 11:40:01 +0100

    chore(tests): add jest-direct-mock (not via jest-fetch-mock)

---
+++ tests/runtimes/node/src/jest-direct-mock.spec.ts
@@ -0,0 +1,26 @@
+"use strict";
+import { describe, test, expect, jest } from "@jest/globals";
+
+// import createCachingMock from "fetch-mock-cache"
+// import FMCMemoryStore from "fetch-mock-cache/stores/memory";
+// @ts-expect-error: .js
+import createCachingFetch from "../../../../src/index";
+// @ts-expect-error: .js
+import FMCMemoryStore from "../../../../src/stores/memory";
+
+describe("jest-fetch-mock", () => {
+  const fetchCache = createCachingFetch({ store: new FMCMemoryStore() });
+  const url = "http://echo.jsontest.com/key/value/one/two";
+  const expectedResponse = { one: "two", key: "value" };
+  global.fetch = jest.fn(fetchCache);
+
+  test("memoryStore", async () => {
+    for (let i = 0; i < 2; i++) {
+      const response = await fetch(url);
+      const data = await response.json();
+      const expectedCacheHeader = i === 0 ? "MISS" : "HIT";
+      expect(response.headers.get("X-FMC-Cache")).toBe(expectedCacheHeader);
+      expect(data).toEqual(expectedResponse);
+    }
+  });
+});
