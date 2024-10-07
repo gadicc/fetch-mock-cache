8f8ff7a2a8d45bf33c3812ba2fdcab2fe804816d
Gadi Cohen <dragon@wastelands.net>
2024-10-07 12:23:15 +0100

    feat(vitest): add vitest-fetch-mock test/example

---
+++ tests/runtimes/node/src/vitest-fetch-mock.spec.ts
@@ -0,0 +1,31 @@
+"use strict";
+import { describe, test, expect, vi } from "vitest";
+import createFetchMock from "vitest-fetch-mock";
+
+// import createCachingMock from "fetch-mock-cache"
+// import FMCMemoryStore from "fetch-mock-cache/stores/memory";
+// @ts-expect-error: .js
+import createCachingFetch from "../../../../src/index";
+// @ts-expect-error: .js
+import FMCMemoryStore from "../../../../src/stores/memory";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+describe("jest-fetch-mock", () => {
+  const fetchCache = createCachingFetch({ store: new FMCMemoryStore() });
+  const url = "http://echo.jsontest.com/key/value/one/two";
+  const expectedResponse = { one: "two", key: "value" };
+
+  test("memoryStore", async () => {
+    for (let i = 0; i < 2; i++) {
+      // If you get a TypeError here, make sure @types/jest is instaled.
+      fetchMock.mockImplementationOnce(fetchCache);
+      const response = await fetch(url);
+      const data = await response.json();
+      const expectedCacheHeader = i === 0 ? "MISS" : "HIT";
+      expect(response.headers.get("X-FMC-Cache")).toBe(expectedCacheHeader);
+      expect(data).toEqual(expectedResponse);
+    }
+  });
+});
